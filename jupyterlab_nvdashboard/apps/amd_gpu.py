a140c590330f2a5d3c650b39b11b6f5f3ec2b270
Johannes Bastian <johannes.bastian@northern.ag>
2021-05-26 12:44:26 +0200

    Decoded all bytes objects in utf-8 strings so that all regular expressions will match.

--- jupyterlab_nvdashboard/apps/amd_gpu.py
+++ jupyterlab_nvdashboard/apps/amd_gpu.py
@@ -21,9 +21,9 @@ class AmdGpuProperties:
     def get_gpu_count(self) -> int:
         """Get the number of working GPUs."""
         try:
-            output = self.bash.run("rocm-smi", capture_output=True).stdout
+            output = self.bash.run("rocm-smi", capture_output=True).stdout.decode("utf-8")
             if "ROCm System Management Interface" in output:
-                gpus_count = [int(num) for num in re.findall(r'[0-9]* ', str(output)) if num != " "]
+                gpus_count = [int(num) for num in re.findall(r'[0-9]* ', output) if num != " "]
                 if len(gpus_count) == 0:
                     return -1
                 else:
@@ -38,9 +38,9 @@ class AmdGpuProperties:
     def get_gpu_utilization(self, flag="-u") -> List[int]:
         """Return the utilization of each GPU in %."""
         try:
-            output = self.bash.run(["rocm-smi", flag], capture_output=True).stdout
+            output = self.bash.run(["rocm-smi", flag], capture_output=True).stdout.decode("utf-8")
             if "ROCm System Management Interface" in output:
-                util = [int(num[:-1]) for num in re.findall(r' [0-9]*\n', str(output))]
+                util = [int(num[:-1]) for num in re.findall(r' [0-9]*\n', output)]
                 return util
             else:
                 return [-1 for _ in range(self.gpus)]
@@ -50,9 +50,9 @@ class AmdGpuProperties:
     def get_gpu_clock_freq(self, flag="-g") -> List[int]:
         """Return the clock frequence of each GPU in Mhz."""
         try:
-            output = self.bash.run(["rocm-smi", flag], capture_output=True).stdout
+            output = self.bash.run(["rocm-smi", flag], capture_output=True).stdout.decode("utf-8")
             if "ROCm System Management Interface" in output:
-                freq = [int(num[1:]) for num in re.findall(r'\([0-9]*', str(output))]
+                freq = [int(num[1:]) for num in re.findall(r'\([0-9]*', output)]
                 return freq
             else:
                 return [-1 for _ in range(self.gpus)]
@@ -62,9 +62,9 @@ class AmdGpuProperties:
     def get_gpu_mem_use(self, flag="--showmemuse") -> List[int]:
         """Return the current memory usage of each GPU in %."""
         try:
-            output = self.bash.run(["rocm-smi", flag], capture_output=True).stdout
+            output = self.bash.run(["rocm-smi", flag], capture_output=True).stdout.decode("utf-8")
             if "ROCm System Management Interface" in output:
-                memUse = [int(num[:-1]) for num in re.findall(r' [0-9]*\n', str(output))]
+                memUse = [int(num[:-1]) for num in re.findall(r' [0-9]*\n', output)]
                 return memUse
             else:
                 return [-1 for _ in range(self.gpus)]
@@ -74,9 +74,9 @@ class AmdGpuProperties:
     def get_gpu_pcie_bandwith(self, flag="-b") -> List[float]:
         """Return the estimated maximum PCIe bandwith in MB/s."""
         try:
-            output = self.bash.run(["rocm-smi", flag], capture_output=True).stdout      # takes a few seconds
+            output = self.bash.run(["rocm-smi", flag], capture_output=True).stdout.decode("utf-8")
             if "ROCm System Management Interface" in output:
-                pcie_use = [float(num[:-1]) for num in re.findall(r' [0-9]*\.[0-9]*\n', str(output))]
+                pcie_use = [float(num[:-1]) for num in re.findall(r' [0-9]*\.[0-9]*\n', output)]
                 return pcie_use
             else:
                 return [-1.0 for _ in range(self.gpus)]
@@ -86,9 +86,9 @@ class AmdGpuProperties:
     def get_gpu_voltage(self, flag="--showvoltage") -> List[int]:
         """Return the current Voltage per GPU in mV."""
         try:
-            output = self.bash.run(["rocm-smi", flag], capture_output=True).stdout      # takes a few seconds
+            output = self.bash.run(["rocm-smi", flag], capture_output=True).stdout.decode("utf-8")
             if "ROCm System Management Interface" in output:
-                voltage = [int(num[:-1]) for num in re.findall(r' [0-9]*\n', str(output))]
+                voltage = [int(num[:-1]) for num in re.findall(r' [0-9]*\n', output)]
                 return voltage
             else:
                 return [-1 for _ in range(self.gpus)]
